b58f140f715ece751245fa0433f3b7c8623970ca
Øyvind Thoner <oyvind.thoner@confirmit.com>
2017-06-26 16:17:15 +0200

    Update for demo

--- LinkedCodeLibrary_Public.js
+++ LinkedCodeLibrary_Public.js
@@ -1,6 +1,6 @@
 class LinkedCodeLibrary_Public {
   static function ReturnText() {
-    return "A text returned from a linked code library";
+    return "A text returned from a linked code library - Demo";
   }
   
   static function AddNumbers(a, b) {
